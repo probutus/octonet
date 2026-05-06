a2a58bafd6ea858423e7bba68bb04a775440cf42
tux <tux@nas.universe>
2026-05-06 22:08:57 +0200

    fix image generation for os image

--- buildroot.octonet/board/digitaldevices/octonet/post-build.sh
+++ buildroot.octonet/board/digitaldevices/octonet/post-build.sh
@@ -2,6 +2,9 @@ TARGETDIR=$1
 
 grep -q config $TARGETDIR/etc/fstab || echo -e "/dev/ubi0_0\t\t/config\tubifs\tdefaults\t0\t0" >> $TARGETDIR/etc/fstab
 grep -q pts $TARGETDIR/etc/securetty || echo -e "pts/0\npts/1\npts/2\npts/3" >> $TARGETDIR/etc/securetty
+grep -q console $TARGETDIR/etc/securetty || echo -e "console" >> $TARGETDIR/etc/securetty
+
+mv $TARGETDIR/boot/uImage.at91-octonet $TARGETDIR/boot/uImage
 
 mkdir -p $TARGETDIR/config
 
