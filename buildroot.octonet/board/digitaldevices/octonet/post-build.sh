2fc91dcfc0fe94bd2bb499bfd8655592b6fa6f4b
tux <tux@nas.universe>
2026-05-17 20:40:35 +0200

    fix access rights for fancontrol.lua

--- buildroot.octonet/board/digitaldevices/octonet/post-build.sh
+++ buildroot.octonet/board/digitaldevices/octonet/post-build.sh
@@ -4,6 +4,10 @@ grep -q config $TARGETDIR/etc/fstab || echo -e "/dev/ubi0_0\t\t/config\tubifs\td
 grep -q pts $TARGETDIR/etc/securetty || echo -e "pts/0\npts/1\npts/2\npts/3" >> $TARGETDIR/etc/securetty
 grep -q console $TARGETDIR/etc/securetty || echo -e "console" >> $TARGETDIR/etc/securetty
 
+if [ -e "$TARGETDIR/var/monitor/fancontrol.lua" ]; then 
+    chmod 755 "$TARGETDIR/var/monitor/fancontrol.lua"
+fi
+
 mv $TARGETDIR/boot/uImage.at91-octonet $TARGETDIR/boot/uImage
 
 mkdir -p $TARGETDIR/config
