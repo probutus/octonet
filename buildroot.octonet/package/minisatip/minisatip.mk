3996f18a88ba7ed6ec1d46f6b31a00cae54da2c4
tux <tux@nas.universe>
2026-06-06 22:37:50 +0200

    Change to minisatip 2.0.81

--- buildroot.octonet/package/minisatip/minisatip.mk
+++ buildroot.octonet/package/minisatip/minisatip.mk
@@ -1,25 +1,30 @@
-MINISATIP_VERSION = v2.0.79
-MINISATIP_SITE = $(call github,catalinii,minisatip,$(MINISATIP_VERSION))
+MINISATIP_VERSION = master
+# Falls v2.0.81 bei probutus nicht existiert, durch Commit-Hash ersetzen, z.B.:
+# MINISATIP_VERSION = abc123def456...
+
+MINISATIP_SITE = $(call github,probutus,minisatip,$(MINISATIP_VERSION))
 MINISATIP_LICENSE = GPL-2.0
 MINISATIP_LICENSE_FILES = LICENSE
-# libbacktrace zu den Abhängigkeiten hinzufügen
+
+# Abhängigkeiten für den Buildprozess
 MINISATIP_DEPENDENCIES = openssl libbacktrace
 
-MINISATIP_CONF_OPTS = -DENABLE_STACKTRACE=OFF -DCMAKE_CXX_STANDARD=23
+# CMake-Konfigurationsoptionen
+MINISATIP_CONF_OPTS = \
+	-DENABLE_STACKTRACE=OFF \
+	-DCMAKE_CXX_STANDARD=23 \
+	-DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS)" \
+	-DCMAKE_EXE_LINKER_FLAGS="-lstdc++exp -lbacktrace"
 
-# Linker-Flags von Compiler-Flags trennen!
-# -lbacktrace gehört in die LINKER_FLAGS, damit der Compiler-Test nicht fehlschlägt.
-MINISATIP_CONF_OPTS += -DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS)"
-MINISATIP_CONF_OPTS += -DCMAKE_EXE_LINKER_FLAGS="-lstdc++exp -lbacktrace"
-
+# ACHTUNG: Die folgenden Zeilen MÜSSEN mit einem TAB eingerückt sein!
 define MINISATIP_REMOVE_STACKTRACE_CODE
-        sed -i 's/.*std::stacktrace.*/\/\/ &/' $(@D)/src/utils.cpp
-        sed -i 's/.*std::to_string(trace).*/\/\/ &/' $(@D)/src/utils.cpp
+	sed -i 's/.*std::stacktrace.*/\/\/ &/' $(@D)/src/utils.cpp
+	sed -i 's/.*std::to_string(trace).*/\/\/ &/' $(@D)/src/utils.cpp
 endef
 
 define MINISATIP_INSTALL_HTML
-        mkdir -p $(TARGET_DIR)/var/satip/www
-        cp -r $(@D)/html/* $(TARGET_DIR)/var/satip/www/
+	mkdir -p $(TARGET_DIR)/var/satip/www
+	cp -r $(@D)/html/* $(TARGET_DIR)/var/satip/www/
 endef
 
 MINISATIP_POST_PATCH_HOOKS += MINISATIP_REMOVE_STACKTRACE_CODE
